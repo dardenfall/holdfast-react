db0d4cb17edb4112a85c9c6c21c30e4de479cc00
Tony Scavone <ascavone@mediamath.com>
2018-09-04 11:51:55 -0400

    added some key handling but movement doesn't work because state updates aren't being recognized

--- my-app/src/components/Game.js
+++ my-app/src/components/Game.js
@@ -20,15 +20,75 @@ class Game extends Component {
         }
       }
     }
+
+    this.setUpButtonHandler();
+  }
+
+  setUpButtonHandler(){
+    window.addEventListener('keydown',(e) => {
+      switch (e.which) {
+        case 37:
+          this.moveLeft();
+          break;
+        case 65:
+          this.moveLeft();
+          break;
+        case 38:
+          this.moveUp();
+          break;
+        case 87:
+          this.moveUp();
+          break;
+        case 39:
+          this.moveRight();
+          break;
+        case 68:
+          this.moveRight();
+          break;
+        case 40:
+          this.moveDown();
+          break;
+        case 83:
+          this.moveDown();
+          break;
+      }      
+    })
+  }
+
+  moveDown(){
+    this.updateHeroPosition(0,1)
+  }
+  moveUp(){
+    this.updateHeroPosition(0,-1)
+  }
+  moveLeft(){
+    this.updateHeroPosition(-1,0)
+  }
+  moveRight(){
+    this.updateHeroPosition(1,0)
+  }
+
+  updateHeroPosition(xdelta, ydelta){
+    console.log("old x", this.state.game.hero.location.x)
+    console.log("old y", this.state.game.hero.location.y)
+    this.state.game.hero.location.x += xdelta; 
+    this.state.game.hero.location.y += ydelta; 
+    console.log("new x", this.state.game.hero.location.x)
+    console.log("new y", this.state.game.hero.location.y)
+  }
+
+  handleButtonPress(e){
+    console.log("THERE IS THE EVENT", this, e)
   }
 
   render() {
     return (
-      <div> here is Game 
+      <div onKeyDown={this.handleButtonPress}> here is Game 
         <Map game={this.state.game}></Map>
       </div>
     );
   }
+
 }
 
 export default Game;

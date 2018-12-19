d049723431ad4f58603b3009228ca3af0bd8ab79
Tony Scavone <ascavone@mediamath.com>
2018-12-19 07:43:07 -0500

    bounce research, pre react-scripts eject

--- my-app/src/components/tiles/Hero.js
+++ my-app/src/components/tiles/Hero.js
@@ -1,29 +1,43 @@
 import React from 'react';
 import Util from '../../Util';
 
-const Hero = (props) => {
-  let cn = "";
-
-  switch (props.direction) {
-    case Util.DIRECTION.RIGHT:
-      cn = "hero-right";
-      break;
-    case Util.DIRECTION.LEFT:
-      cn = "hero-left";
-      break;
-    case Util.DIRECTION.DOWN:
-      cn = "hero-down";
-      break;
-    case Util.DIRECTION.UP:
-      cn = "hero-up";
-      break;
-  
-    default:
-      throw Error("NO DIRECTION PASSED TO HERO")
+class Hero extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.myRef = React.createRef();
   }
-  return (
-    <div className={cn}></div>
-  );
+
+  render(){
+    let cn = "";
+
+    switch (this.props.direction) {
+      case Util.DIRECTION.RIGHT:
+        cn = "hero-right";
+        break;
+      case Util.DIRECTION.LEFT:
+        cn = "hero-left";
+        break;
+      case Util.DIRECTION.DOWN:
+        cn = "hero-down";
+        break;
+      case Util.DIRECTION.UP:
+        cn = "hero-up";
+        break;
+    
+      default:
+        throw Error("NO DIRECTION PASSED TO HERO")
+    }
+  
+    return (
+      <div className={cn}></div>
+    );
+  }
+
+  componentDidMount() {
+//    $(this.myRef).effect( "bounce", {times:3}, 300 ); // eslint-disable-line
+console.log("component mounted")
+  }
+  
 }
 
 
